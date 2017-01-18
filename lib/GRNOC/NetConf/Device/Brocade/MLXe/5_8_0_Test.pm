f3787e5a06677764fc41bd86cb0d1055c7906e6a
Jonathan Stout <jonstout@grnoc.iu.edu>
2017-01-18 22:08:48 +0000

    Adding a couple test cases for get_interfaces

--- lib/GRNOC/NetConf/Device/Brocade/MLXe/5_8_0_Test.pm
+++ lib/GRNOC/NetConf/Device/Brocade/MLXe/5_8_0_Test.pm
@@ -7,12 +7,40 @@ use warnings;
 
 use Moo;
 use AnyEvent;
+use Data::Dumper;
 use XML::Writer;
 use XML::Simple;
 
 extends 'GRNOC::NetConf::Device::Brocade::MLXe::5_8_0';
 
-has responseChan => (is => 'rwp');
+has responseChan      => (is => 'rwp');
+has responseXmlString => (is => 'rwp', default => '');
+
+=head2 set_response
+
+Gives user the ability to create responses that the device might
+return for a given request. After the response has been received it is
+discarded; In otherwords if you want to get the same response twice
+you'll need to call this method twice.
+
+=cut
+sub set_response {
+    my $self    = shift;
+    my $payload = shift;
+
+    $self->_set_responseXmlString($payload);
+    return 1;
+}
+
+=head2 _connect
+
+Makes the device think it's connected via SSH.
+
+=cut
+sub _connect {
+    my $self = shift;
+    return 1;
+}
 
 =head2 send
 
@@ -39,6 +67,10 @@ sub recv {
 
     my $chan   = $self->responseChan;
     my $string = $chan->recv();
+    if ($self->responseXmlString ne '') {
+        $string = $self->responseXmlString;
+        $self->_set_responseXmlString('');
+    }
 
     my $parser = XML::Simple->new();
     my $xml    = $parser->XMLin($string);
